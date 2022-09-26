cc72e82002e96dc40885e53f91621f244c1a831d
Tigran Karapetyan <tigran4joystick@gmail.com>
2022-09-26 22:48:06 +0400

    Section 6: Styling react components: complete

--- 01-starting-project/01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 01-starting-project/01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,34 @@
 import React, { useState } from "react";
 
 import Button from "../../UI/Button/Button";
-import "./CourseInput.css";
+import styles from "./CourseInput.module.css";
+
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
+
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//   }
+
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid #ccc;
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//     background: ${(props) => (props.invalid ? "#ffd7d7" : "transparent")};
+//   }
+
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
+
+// `;
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -30,12 +57,13 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div
+        className={`${styles["form-control"]} ${!isValid && styles.invalid}`}
+      >
         <label style={{ color: validationColor }}>Course Goal</label>
         <input
           style={{
             borderColor: validationColor,
-            background: isValid ? "transparent" : "salmon",
           }}
           type="text"
           onChange={goalInputChangeHandler}
