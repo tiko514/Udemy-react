40567b196fb6ed507401b6b6a3ce9a768bc05c83
Tigran Karapetyan <tigran4joystick@gmail.com>
2022-10-02 01:48:42 +0400

    UsersList component with Add User functionality

--- user-manager/src/App.js
+++ user-manager/src/App.js
@@ -1,14 +1,19 @@
-import './App.css';
-import AddUser from './Components/Users/AddUser';
+import { useState } from "react";
+import "./App.css";
+import AddUser from "./Components/Users/AddUser";
+import UsersList from "./Components/Users/UsersList";
 
 function App() {
   const saveUserDataHandler = (userData) => {
-    console.log(userData);
+    setUsersList((prevUsersList) => [userData, ...prevUsersList]);
   };
+
+  const [usersList, setUsersList] = useState([]);
 
   return (
     <div className="App">
       <AddUser onSaveUserData={saveUserDataHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
