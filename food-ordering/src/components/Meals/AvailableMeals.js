d36f25e835800fd55ad8a037eca96fde93f1ff2c
Tigran Karapetyan <tigran.karapetyan@macadamian.com>
2022-10-25 16:59:44 +0400

    Getting started with initial components

---
+++ food-ordering/src/components/Meals/AvailableMeals.js
@@ -0,0 +1,36 @@
+
+const DUMMY_MEALS = [
+    {
+        id: 'm1',
+        name: 'Sushi',
+        description: 'Finest fish and veggies',
+        price: 22.99,
+    },
+    {
+        id: 'm2',
+        name: 'Schnitzel',
+        description: 'A german specialty!',
+        price: 16.5,
+    },
+    {
+        id: 'm3',
+        name: 'Barbecue Burger',
+        description: 'American, raw, meaty',
+        price: 12.99,
+    },
+    {
+        id: 'm4',
+        name: 'Green Bowl',
+        description: 'Healthy...and green...',
+        price: 18.99,
+    },
+];
+
+const AvailableMeals = props => {
+    const meals = DUMMY_MEALS.map(m => <li>{m.description}</li>)
+    return <ul>
+        {meals}
+    </ul>
+}
+
+export default AvailableMeals;
