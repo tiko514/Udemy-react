2224c897130647efddae811d783d0b26bf6ac92a
Tigran Karapetyan <tigran4joystick@gmail.com>
2022-11-14 02:19:20 +0400

    Show & hide cart modal

---
+++ food-ordering/src/components/UI/Modal.js
@@ -0,0 +1,34 @@
+import { Fragment } from "react";
+import ReactDOM from "react-dom";
+import classes from "./Modal.module.css";
+
+const Modal = (props) => {
+  const Backdrop = (props) => {
+    return <div className={classes.backdrop} onClick={props.onClose}></div>;
+  };
+
+  const ModalOverlay = (props) => {
+    return (
+      <div className={classes.modal}>
+        <div className={classes.content}>{props.children}</div>
+      </div>
+    );
+  };
+
+  const portalElement = document.getElementById("overlays");
+
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onClose={props.onClose} />,
+        portalElement
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </Fragment>
+  );
+};
+
+export default Modal;
